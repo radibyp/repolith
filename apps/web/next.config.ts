21aaed7bd2969b15f4acc4dd8c6c28d16c935abe
radityprtama <radityprtama@gmail.com>
2026-03-08 15:54:27 +0700

    Chore: remove bun.lock and update sentry config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -101,15 +101,12 @@ export default withSentryConfig(nextConfig, {
 	// For all available options, see:
 	// https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-	org: "repolith",
+	org: "kaltdev",
 
-	project: "javascript-nextjs",
+	project: "repolith",
 
 	// Only print logs for uploading source maps in CI
 	silent: !process.env.CI,
-
-	// For all available options, see:
-	// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
 	// Upload a larger set of source maps for prettier stack traces (increases build time)
 	widenClientFileUpload: true,
